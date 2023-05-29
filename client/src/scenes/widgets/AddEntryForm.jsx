39f2a59084186d68121321b90e7213f7a2df1d02
Joshua Strozzi <joshuastrozzi@gmail.com>
2023-05-29 14:38:55 -0700

    Works in dev environment, but not prod. fix?

--- client/src/scenes/widgets/AddEntryForm.jsx
+++ client/src/scenes/widgets/AddEntryForm.jsx
@@ -39,11 +39,11 @@ const AddEntryForm = ({googleBook}) => {
     const addEntrySchema = yup.object().shape({
         review: yup.string().max(1000).nullable(true),
         startDate: yup.date().min(bookData.released || "1000-01-01", 
-            "Start data can't be before publish date").nullable(true),
+            "Start data can't be before publish date").nullable(true).default(null),
         endDate: yup.date().min(
             yup.ref('startDate'),
             "End date can't be before start date"
-            ).nullable(true),
+            ).nullable(true).default(null),
         rating: yup.number().min(0).max(10).nullable(true),
         status: yup.string().oneOf(["Planning", "Completed", "Reading", "Dropped"]).nullable(false),
         page: yup.number().min(0).max(bookData.pages || 10000).typeError("Must be number").nullable(true)
